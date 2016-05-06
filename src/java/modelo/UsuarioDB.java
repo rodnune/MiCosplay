24047af3d7b0dfdfa0889ec1391ea8c3a229b1aa
rodrigo <rpaul_94@hotmail.com>
2016-05-06 20:20:52 +0200

    añadida consulta para ver si existe el usuario en la BD

--- src/java/modelo/UsuarioDB.java
+++ src/java/modelo/UsuarioDB.java
@@ -16,12 +16,25 @@ public class UsuarioDB {
              conexion.ejecutaUpdate(query);
              conexion.close();
 
-            
-       
     }
-        
-     
     
-     }
+    public static boolean comprobarNick(String nick) throws SQLException, ClassNotFoundException {
+
+            ConexionBD conexion = ConexionBD.getInstancia();
+           
+    String query =  "SELECT NICK FROM USUARIO U WHERE U.NICK = '"+nick+"'";
+             ResultSet rs = conexion.ejecutaRecuperacion(query);
+             if ( rs!=null && rs.next() ){ 
+                 conexion.close();
+                    return true;
+                    }else{
+                 conexion.close();
+                return false;
+             }
+            
+    }
+    
+    
+}
 
   
