f6561ef5029eb666148bc2189fd709fca74b8cf4
rodrigo <rpaul_94@hotmail.com>
2016-05-07 16:13:14 +0200

    Adaptada la consulta de articulo como lo ha hecho Jose

--- src/java/modelo/ArticuloDB.java
+++ src/java/modelo/ArticuloDB.java
@@ -6,14 +6,15 @@ public class ArticuloDB {
     
     public static void insertarArticulo(Articulo article) throws SQLException, ClassNotFoundException {
 
-            ConexionBD conexion = ConexionBD.getInstancia();
-         
-    String query ="INSERT INTO ARTICULO (nombre,nick,precio,estadoArticulo,descripcion,categoria,subcategoria) "
-            + "VALUES ('"+article.getNombre() +"', '"+article.getUser().getNick()+"', " +article.getPrecio()+", '"+article.getEstadoArticulo()+"', '"+article.getDesc()+"' , '"+article.getCategoria()+"' ,'"+article.getSubcategoria()+"')";
-    
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+        try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/MiCosplayDB", "root", "cosplay")) {
+            Statement stmt = conn.createStatement();
+            String query ="INSERT INTO ARTICULO (nombre,nick,precio,estadoArticulo,descripcion,categoria,subcategoria) "
+                    + "VALUES ('"+article.getNombre() +"', '"+article.getUser().getNick()+"', " +article.getPrecio()+", '"+article.getEstadoArticulo()+"', '"+article.getDesc()+"' , '"+article.getCategoria()+"' ,'"+article.getSubcategoria()+"')";
             
-             conexion.ejecutaUpdate(query);
-             conexion.close();
+            stmt.executeUpdate(query);
+            stmt.close();
+        }
 
     }
     
